36e0bedae8dea18553e4875b7657c07a1afad2aa
vprvix <vprking1@gmail.com>
2020-04-11 14:43:38 +0100

    Made the db saving asynchronous for at home context

--- app/src/main/java/strathclyde/contextualtriggers/context/location/AtHouseContext.kt
+++ app/src/main/java/strathclyde/contextualtriggers/context/location/AtHouseContext.kt
@@ -1,7 +1,10 @@
 package strathclyde.contextualtriggers.context.location
 
 import android.app.Application
+import android.os.AsyncTask
+import android.util.Log
 import strathclyde.contextualtriggers.database.LocationEntry
+import strathclyde.contextualtriggers.database.LocationEntryDao
 import strathclyde.contextualtriggers.database.MainDatabase
 import java.util.*
 import kotlin.collections.HashMap
@@ -12,6 +15,9 @@ class AtHouseContext(
     application
 ) {
     private var history: MutableList<LocationEntry> = LinkedList()
+    private lateinit var locationEntryDao: LocationEntryDao
+    private val dbLoad: DBLoad = DBLoad()
+    val dbSave: DBSave = DBSave()
     private val tolerance: Double = 0.5
     override fun useLocation(lat: Double, long: Double): Int {
         //create region known as home location
@@ -29,9 +35,8 @@ class AtHouseContext(
         val currentLocation = LocationEntry()
         currentLocation.lat = lat
         currentLocation.lon = long
-        MainDatabase.getInstance(super.application).locationEntryDao.update(currentLocation)
-        history = MainDatabase.getInstance(super.application).locationEntryDao.getAll()
         history.add(currentLocation)
+        dbSave.execute(this)
         var home: Pair<Double, Double> = Pair(0.0, 0.0)
         val mapping: MutableMap<Pair<Double, Double>, Int> = HashMap()
         for (current in history) {
@@ -51,14 +56,35 @@ class AtHouseContext(
 
     override fun onStart() {
         super.onStart()
+        locationEntryDao = MainDatabase.getInstance(super.application).locationEntryDao
+        dbLoad.execute(this)
     }
 
     override fun onStop() {
         super.onStop()
-        val dao = MainDatabase.getInstance(super.application).locationEntryDao
-        history.forEach {
-            dao.update(it)
+        dbSave.execute(this)
+    }
+
+    class DBSave : AsyncTask<AtHouseContext, Void, Void?>() {
+        override fun doInBackground(vararg p0: AtHouseContext?): Void? {
+            val master = p0.first()
+            master?.history?.forEach { master.locationEntryDao.update(it) }
+            Log.d("AT HOME CONTEXT", "UwU i save history " + (master?.history))
+            return null
         }
+
+    }
+
+    class DBLoad : AsyncTask<AtHouseContext, Void, Void?>() {
+        override fun doInBackground(vararg p0: AtHouseContext?): Void? {
+            val master = p0.first()
+
+            if (master != null)
+                master.history = master.locationEntryDao.getAll()
+            Log.d("AT HOME CONTEXT", "UwU i got history " + (master?.history))
+            return null
+        }
+
     }
 //𝓤𝔀𝓤
 }
