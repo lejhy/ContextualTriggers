3befeb22384dc4cf11df77d2a1fb310551142687
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2020-04-13 02:18:55 +0200

    Prevent creating triggers with identical title (would be nice to do the duplicate checks properly at some point, this is more like a quick hack)

--- app/src/main/java/strathclyde/contextualtriggers/receiver/TriggersBroadcastReceiver.kt
+++ app/src/main/java/strathclyde/contextualtriggers/receiver/TriggersBroadcastReceiver.kt
@@ -20,55 +20,62 @@ class TriggersBroadcastReceiver : BroadcastReceiver() {
     private val uiScope = CoroutineScope(Dispatchers.Main + receiverJob)
 
     override fun onReceive(context: Context, intent: Intent) {
-        Log.i("Intent", "received")
         when (intent.action) {
             newTriggerAction -> uiScope.launch {
                 try {
-                    val trigger = Trigger(
-                        0L,
-                        intent.getStringExtra("title")!!,
-                        intent.getStringExtra("content")!!,
-                        intent.getStringExtra("iconKey")!!,
-                        intent.getBooleanExtra("active", true)
-                    )
-                    if (intent.getBooleanExtra("useProgressBar", false)) {
-                        trigger.useProgressBar = true;
-                        trigger.progressContentUri = intent.getStringExtra("progressContentUri")!!
-                    }
-                    val actionKeys = intent.getStringExtra("actionKeys")
-                    actionKeys?.let {
-                        trigger.actionKeys = actionKeys
-                        trigger.actionContentUri = intent.getStringExtra("actionContentUri")!!
-                    }
-                    val contextConstraints = mutableListOf<ContextConstraint>()
-                    val contextKeyList = intent.getStringArrayListExtra("contextKeyList")!!
-                    val greaterThanOrEqualToList = intent.getIntegerArrayListExtra("greaterThanOrEqualToList")!!
-                    val lessThanOrEqualToToList = intent.getIntegerArrayListExtra("lessThanOrEqualToList")!!
-                    for ((index, contextKey) in contextKeyList.withIndex()) {
-                        contextConstraints.add(
-                            ContextConstraint(
-                                0L,
-                                contextKey,
-                                greaterThanOrEqualToList[index],
-                                lessThanOrEqualToToList[index],
-                                0L
-                            )
-                        )
-                    }
-                    val triggerWithContextConstraints = TriggerWithContextConstraints(
-                        trigger,
-                        contextConstraints
-                    )
+                    val title = intent.getStringExtra("title")!!
                     val datasource = MainDatabase.getInstance(context)
-                    withContext(Dispatchers.IO) {
-                        datasource.triggerWithContextConstraintsDao.insertTriggerWithContextConstraints(
-                            triggerWithContextConstraints
-                        )
-                        datasource.triggerWithContextConstraintsDao.get(trigger.title)?.let {
-                            Log.i(it.trigger.title, it.trigger.content)
-                        }
+                    val triggerWithContextConstraints = withContext(Dispatchers.IO) {
+                        datasource.triggerWithContextConstraintsDao.get(title)
                     }
-                    Log.i("TriggersBroadcastReceiver", "New trigger created...")
+                    if (triggerWithContextConstraints == null) {
+                        val trigger = Trigger(
+                            0L,
+                            title,
+                            intent.getStringExtra("content")!!,
+                            intent.getStringExtra("iconKey")!!,
+                            intent.getBooleanExtra("active", true)
+                        )
+                        if (intent.getBooleanExtra("useProgressBar", false)) {
+                            trigger.useProgressBar = true;
+                            trigger.progressContentUri = intent.getStringExtra("progressContentUri")!!
+                        }
+                        val actionKeys = intent.getStringExtra("actionKeys")
+                        actionKeys?.let {
+                            trigger.actionKeys = actionKeys
+                            trigger.actionContentUri = intent.getStringExtra("actionContentUri")!!
+                        }
+                        val contextConstraints = mutableListOf<ContextConstraint>()
+                        val contextKeyList = intent.getStringArrayListExtra("contextKeyList")!!
+                        val greaterThanOrEqualToList = intent.getIntegerArrayListExtra("greaterThanOrEqualToList")!!
+                        val lessThanOrEqualToToList = intent.getIntegerArrayListExtra("lessThanOrEqualToList")!!
+                        for ((index, contextKey) in contextKeyList.withIndex()) {
+                            contextConstraints.add(
+                                ContextConstraint(
+                                    0L,
+                                    contextKey,
+                                    greaterThanOrEqualToList[index],
+                                    lessThanOrEqualToToList[index],
+                                    0L
+                                )
+                            )
+                        }
+                        val triggerWithContextConstraints = TriggerWithContextConstraints(
+                            trigger,
+                            contextConstraints
+                        )
+                        withContext(Dispatchers.IO) {
+                            datasource.triggerWithContextConstraintsDao.insertTriggerWithContextConstraints(
+                                triggerWithContextConstraints
+                            )
+                            datasource.triggerWithContextConstraintsDao.get(trigger.title)?.let {
+                                Log.i(it.trigger.title, it.trigger.content)
+                            }
+                        }
+                        Log.i("TriggersBroadcastReceiver", "New trigger created...")
+                    } else {
+                        Log.i("TriggersBroadcastReceiver", "Trigger already exists...")
+                    }
                 } catch (e: NullPointerException) {
                     Log.i("TriggersBroadcastReceiver", "Failed to create trigger...")
                 }
